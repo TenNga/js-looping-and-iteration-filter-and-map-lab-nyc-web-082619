eb3b8064d82ff3b03c617773f7027f6cac235da4
Kevin Karma <kevinkarma55@gmail.com>
2019-10-06 22:08:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution here:
+function driversWithRevenueOver(driver,revenue) {
+  var result = []
+ for(var i = 0; i < driver.length; i++) {
+   if (driver.revenue > revenue)
+      result.add(driver)
+ }
+ return result;
+}
+
+function driverNamesWithRevenueOver(driver, revenue) {
+  var result = []
+ for(var i = 0; i < driver.length; i++) {
+   if (driver.revenue > revenue)
+      result.add(driver.name)
+ }
+ return result;
+}
+
+function exactMatch(driver,attr) {
+  var result = []
+  attr_key = Object.keys(attr)[1]
+ for(var i = 0; i < driver.length; i++) {
+   keys = Object.keys(driver)
+   
+   if (keys[i] == attr_key)
+      result.add(driver)
+ }
+ return result;
+}
